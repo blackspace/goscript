574e0bba594b34bff3bbbb254de30a18256268f7
weiwufeng <weiwufeng@sohu.com>
2016-09-11 12:45:11 +0800

    ...

--- vm/runtime/runtime.go
+++ vm/runtime/runtime.go
@@ -5,40 +5,84 @@ import (
 )
 
 type Runtime struct {
-	Scopes []*Scope
+	ScopeListHead *ScopeNode
+}
+
+type ScopeNode struct {
+	pre *ScopeNode
+	next *ScopeNode
+	*Scope
+}
+
+func NewScopeNode() *ScopeNode {
+	return &ScopeNode{Scope:NewScope()}
 }
 
 
 func NewRuntime() (r *Runtime) {
-	return &Runtime{Scopes:make([]*Scope,0,1000)}
+	return &Runtime{}
+}
+
+func (r *Runtime)LastScopeNode() (sn *ScopeNode) {
+	sn=r.ScopeListHead
+	for {
+		if sn==nil  {
+			return sn
+		} else if sn.next==nil {
+			return sn
+		}
+		sn=sn.next
+	}
+
+	return
+}
+
+func (r *Runtime)FindScopeNode(s *Scope) (sn *ScopeNode) {
+	for sn=r.ScopeListHead;sn.next!=nil;sn=sn.next {
+		if sn.Scope==s {
+			return
+		}
+	}
+	return
 }
 
 
 
 func (r *Runtime)BeginScope() (s *Scope){
-	s=NewScope()
-	r.Scopes=append(r.Scopes,s)
-	return
+	sn:=NewScopeNode()
+
+	if r.ScopeListHead==nil {
+		r.ScopeListHead=sn
+	} else {
+		sn.pre=r.LastScopeNode()
+		r.LastScopeNode().next=sn
+	}
+
+	return sn.Scope
 }
 
 
 func (r *Runtime)EndScope(s *Scope) {
-	for i,ls:=range r.Scopes {
-		if ls==s {
-			r.Scopes[i]=nil
-		}
+	n:=r.FindScopeNode(s)
+
+	if n.pre!=nil {
+		n.pre.next = n.next
+	}
+
+	if n.next!=nil {
+		n.next.pre=n.pre
 	}
 }
 
 func (r *Runtime)GetVarible(n string) (v reflect.Value,ok bool) {
-	for i:=len(r.Scopes)-1;i>=0;i-- {
-		s:=r.Scopes[i]
-		if s==nil {
-			continue
+	for sn:=r.LastScopeNode();;sn=sn.pre {
+
+		if v,ok=sn.Get(n);ok {
+			return v,ok
 		}
 
-		if v,ok=s.Get(n);ok {
-			return v,ok
+		if sn.pre==nil  {
+			break
 		}
 	}
 
@@ -46,28 +90,18 @@ func (r *Runtime)GetVarible(n string) (v reflect.Value,ok bool) {
 }
 
 func (r *Runtime)SetVarible(n string,v reflect.Value) {
-	for i:=len(r.Scopes)-1;i>=0;i-- {
-		s:=r.Scopes[i]
-		if s==nil {
-			continue
-		}
+	for sn:=r.LastScopeNode();;sn=sn.pre {
 
-		if _,ok:=s.Get(n);ok {
-			s.Set(n,v)
+		if _,ok:=sn.Get(n);ok {
+			sn.Set(n,v)
 			return
 		}
-	}
 
-	for i:=len(r.Scopes)-1;i>=0;i-- {
-		s := r.Scopes[i]
-		if s == nil {
-			continue
+		if sn.pre==nil  {
+			break
 		}
-
-		s.Set(n, v)
-		return
 	}
 
-
-
+	r.LastScopeNode().Set(n, v)
+	return
 }
