1bdd5de5bcc7ff3163fd3a01395758ad6c4375ba
weiwufeng <weiwufeng@sohu.com>
2016-09-25 09:47:00 +0800

    ...

--- vm/test/vm_object_test.go
+++ vm/test/vm_object_test.go
@@ -20,12 +20,28 @@ func TestObject(t *testing.T) {
 	}
 
 
-	vm.Run("b={a:1,b:2,c:3}")
+	vm.Run("b={a:1,b:{a:1,b:2},c:[1,3]}")
 
-	v,_=vm.Run("b.b")
+	v,_=vm.Run("b.b.b")
 
 
 	if v.(int64)!=2 {
 		t.Fail()
 	}
+
+	v,_=vm.Run("b.c[1]")
+
+
+	if v.(int64)!=3 {
+		t.Fail()
+	}
+
+	vm.Run("b.b.f=lambda() {this.a}")
+
+	v,_=vm.Run("b.b.f()")
+
+	if v.(int64)!=1 {
+		t.Fail()
+	}
 }
+
